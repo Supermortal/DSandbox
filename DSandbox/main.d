8dfd71573f9b78ef0945265e8a5c866b0752426b
Christopher Pink <Christopher.Pink@extendhealth.com>
2015-09-04 09:13:02 -0600

    Added some functions

--- DSandbox/main.d
+++ DSandbox/main.d
@@ -1,8 +1,39 @@
 import std.stdio;
+import std.math;
+
+int magicNumber = 42;
+string password = "seasme";
 
 int main(string[] argv)
 {
-    writeln("Hello D-World!");
+	int testInt;
+
+    if (testInt == 0) {
+			writefln("testInt is 0");
+	}
+	else {
+			writefln("testInt is not 0");
+	}
+
+	initValues();
+	stringStuff();
+
+	double testD = sqrt(4f);
+	writefln("%s", testD);
+
 	readf("");
     return 0;
+}
+
+void initValues() {
+	writeln(int.init);
+	writeln(float.init);
+}
+
+void stringStuff() {
+	string str1 = "Some";
+	string str2 = "Stuff";
+	string str3 = str1 ~= " ";
+
+	writefln("%s", str3 ~= str2);
 }
